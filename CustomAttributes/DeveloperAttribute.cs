f0c582101c03c62b8dd3bd1645bad236cff1372a
deoresulakshana@gmail.com <36725147+sulakshanadeore@users.noreply.github.com>
2022-09-12 12:18:18 +0530

    Add project files.

---
+++ CustomAttributes/DeveloperAttribute.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomAttributes
+{
+
+        [System.AttributeUsage(AttributeTargets.All, Inherited = false, AllowMultiple = true)]
+  sealed   class DeveloperAttribute : Attribute
+    {
+        // See the attribute guidelines at 
+        //  http://go.microsoft.com/fwlink/?LinkId=85236
+        readonly string positionalString;
+
+        // This is a positional argument
+        public DeveloperAttribute(string positionalString)
+        {
+            this.positionalString = positionalString;
+
+            // TODO: Implement code here
+
+            throw new NotImplementedException();
+        }
+
+        public string PositionalString
+        {
+            get { return positionalString; }
+        }
+
+        // This is a named argument
+        public int NamedInt { get; set; }
+        public string DeveloperName { get; set; }
+
+    }
+}
